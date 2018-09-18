db7d2182d6269e4c8ac5b5d02bdd814b09402cb6
Kajigga Dev <kajigga@gmail.com>
2018-09-18 09:51:00 -0600

    adjusting css for driver info

--- truckstop_salt_srv/salt/enable_truckstop_tracker/files/app/static/components/driver_details.js
+++ truckstop_salt_srv/salt/enable_truckstop_tracker/files/app/static/components/driver_details.js
@@ -1,10 +1,22 @@
 {% raw %}
 Vue.component('driver-details', {
     props: ['driver'],
-    template: `<div class='driver'>
-    <img :src='driver.picture.medium' width='75px' :alt='driver.name.first +" "+ driver.name.last'/>
-    <span class='name'>{{driver.name.first}}</span> <span class='name'>{{ driver.name.last }}</span>
-    </div>`
+    template: `
+        <div class='driver'>
+            <div class='photo'>
+                <img :src='driver.picture.medium' width='75px' :alt='driver.name.first +" "+ driver.name.last'/>
+            </div>
+            <div class='driver-info'>
+                <span class='name'>{{driver.name.first}}</span> <span class='name'>{{ driver.name.last }}</span>
+                <br/>
+                <span class='phone'>{{ driver.phone }}</span>
+                <br/>
+                <span class='cell'>{{ driver.cell }}</span>
+                <br/>
+                <span class='email'>{{ driver.email }}</span>
+            </div>
+        </div>
+    `
 
 });
 {% endraw %}
