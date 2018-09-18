b95cf180c5a9656b904e6b79db5c73409423ed46
Kajigga Dev <kajigga@gmail.com>
2018-09-18 09:25:29 -0600

    adding some css

--- truckstop_salt_srv/salt/enable_truckstop_tracker/files/app/static/components/driver_details.js
+++ truckstop_salt_srv/salt/enable_truckstop_tracker/files/app/static/components/driver_details.js
@@ -1,7 +1,10 @@
 {% raw %}
 Vue.component('driver-details', {
     props: ['driver'],
-    template: `<p>{{driver.name.first}} {{ driver.name.last }}</p>`
+    template: `<div class='driver'>
+    <img :src='driver.picture.medium' width='75px' :alt='driver.name.first +" "+ driver.name.last'/>
+    <span class='name'>{{driver.name.first}}</span> <span class='name'>{{ driver.name.last }}</span>
+    </div>`
 
 });
 {% endraw %}
